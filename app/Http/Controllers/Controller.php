c637d9be664eb2955a0b59ecba827898e5db4a9e
unknown <pinedacj1020@gmail.com>
2023-05-04 23:35:09 +0800

    test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    
+    public function index()
+    {
+
+        //TEST COMMIT
+        $users = User::all();
+    
+        return view('users.index', ['users' => $users]);
+    }
+
 }
